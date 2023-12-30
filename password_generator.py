111ac04b03a0a138b0fcfc20d3a96f69485fdb5a
Mert Yavas <137993034+mert-yavas@users.noreply.github.com>
2023-12-30 15:07:51 +0300

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,31 +1,38 @@
-# Password Generator Project
 import random
 
-# Lists of characters to be used in the password
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+# Define the possible characters for the password
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+# Greeting message for the user
+print("Welcome to the PyPassword Generator!")
 
-def password_generator():
-    # Set the number of letters, symbols, and numbers in the password
-    nr_letters = random.randint(8, 10)
-    nr_symbols = random.randint(2, 4)
-    nr_numbers = random.randint(2, 4)
+# Asking the user for their preferences
+nr_letters = int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
 
-    # Generate random characters for each category
-    password_letters = [random.choice(letters) for _ in range(nr_letters)]
-    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
-    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+# Initialize an empty list to store the password elements
+password = []
 
-    # Combine and shuffle the characters
-    password_list = password_numbers + password_letters + password_symbols
-    random.shuffle(password_list)
+# Adding random letters to the password
+for i in range(nr_letters):
+    password.append(random.choice(letters))
 
-    # Create the final password by joining the characters
-    password = "".join(password_list)
+# Adding random symbols to the password
+for i in range(nr_symbols):
+    password.append(random.choice(symbols))
 
-    return password
+# Adding random numbers to the password
+for i in range(nr_numbers):
+    password.append(random.choice(numbers))
 
+# Shuffling the password to ensure randomness
+random.shuffle(password)
+
+# Converting the list of characters into a single string
+shuffled_password_no_comma = "".join(password)
+
+# Display the final password to the user
+print(f"Your password is: {shuffled_password_no_comma}")
